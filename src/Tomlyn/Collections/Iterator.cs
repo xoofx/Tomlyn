8f997483d3df29ee0ae217d75bad851ebc2ec0aa
Alexandre Mutel <alexandre@unity3d.com>
2019-02-12 22:00:29 +0100

    Update license headers

--- src/Tomlyn/Collections/Iterator.cs
+++ src/Tomlyn/Collections/Iterator.cs
@@ -1,4 +1,4 @@
-// Copyright (c) 2019 - Alexandre Mutel. All rights reserved.
+// Copyright (c) Alexandre Mutel. All rights reserved.
 // Licensed under the BSD-Clause 2 license. 
 // See license.txt file in the project root for full license information.
 namespace Tomlyn.Collections
--- src/Tomlyn/Text/CharacterIterator.cs
+++ src/Tomlyn/Text/CharacterIterator.cs
@@ -1,7 +1,6 @@
-// Copyright (c) 2019 - Alexandre Mutel. All rights reserved.
+// Copyright (c) Alexandre Mutel. All rights reserved.
 // Licensed under the BSD-Clause 2 license. 
 // See license.txt file in the project root for full license information.
-
 using Tomlyn.Collections;
 
 namespace Tomlyn.Text
