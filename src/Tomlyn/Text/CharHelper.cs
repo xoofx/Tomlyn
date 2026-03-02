2926365ca4638a3797bed761241f9d80053f860a
Alexandre Mutel <alexandre_mutel@live.com>
2026-03-02 21:45:19 +0100

    Fix Unicode scalar validation for U+10FFFF

--- src/Tomlyn/Text/CharHelper.cs
+++ src/Tomlyn/Text/CharHelper.cs
@@ -50,7 +50,7 @@ namespace Tomlyn.Text
 
         public static bool IsValidUnicodeScalarValue(char32 c)
         {
-            return c >= 0 && c <= 0xD7FF || c >= 0xE000 && c < 0x10FFFF;
+            return c >= 0 && c <= 0xD7FF || c >= 0xE000 && c <= 0x10FFFF;
         }
 
         public static bool IsDigit(char32 c)
