120a984af8d50900d2e8df17e7c676b2e3782225
Alexandre Mutel <alexandre_mutel@live.com>
2026-03-03 07:37:37 +0100

    Restructure benchmark runner

--- src/Tomlyn.Benchmarks/Tomlyn.Benchmarks/Program.cs
+++ src/Tomlyn.Benchmarks/Tomlyn.Benchmarks/Program.cs
@@ -1,4 +1,15 @@
-﻿using BenchmarkDotNet.Running;
-using Tomlyn.Benchmarks;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Order;
+using BenchmarkDotNet.Running;
 
-BenchmarkRunner.Run<BasicBenchmark>();
+var config = ManualConfig.Create(DefaultConfig.Instance)
+    .WithOptions(ConfigOptions.JoinSummary)
+    .WithOrderer(new DefaultOrderer(SummaryOrderPolicy.Declared))
+    .AddColumn(CategoriesColumn.Default)
+    .AddLogicalGroupRules(BenchmarkLogicalGroupRule.ByCategory);
+
+BenchmarkSwitcher
+    .FromAssembly(typeof(Program).Assembly)
+    .Run(args, config);
+
